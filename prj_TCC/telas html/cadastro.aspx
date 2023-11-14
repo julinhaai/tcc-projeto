cb37e5ceaeea9215c5b419a0ee9d5d594a1ed9ad
julinhaai <julia.santos855@etec.sp.gov.br>
2023-11-14 11:26:27 -0300

    cadastrto

--- prj_TCC/telas html/cadastro.aspx
+++ prj_TCC/telas html/cadastro.aspx
@@ -9,7 +9,50 @@
     <title>Ideias Vivas</title>
 </head>
 <body>
-   
+   <div class="box">
+       <div class="img-box">
+           <img src="img/form-img.png" alt=" " />
+       </div>
+       <div class="form-box">
+           <h2>Criar conta</h2>
+           <p>Já é um membro? <a href="#">Login</a></p>
+           <form id="form1" runat="server">
+               <div class="input-group">
+                   <asp:Label ID="lblNome" runat="server"  CssClass="label" Text="Nome Completo"></asp:Label>
+                   <asp:TextBox ID="txtNome" runat="server"  CssClass="text" placeholder="Digite o seu nome completo"></asp:TextBox>
+               </div>
+
+               <div class="input-group">
+                   <asp:Label ID="lblEmail" runat="server" CssClass="label" Text="Email"></asp:Label>
+                   <asp:TextBox ID="txtEmail" runat="server" CssClass="text" placeholder="Digite o seu Email"></asp:TextBox>
+               </div>
+
+               <div class="input-group">
+                   <asp:Label ID="lblCPFouCNPJ" runat="server" CssClass="label" Text="Selecione:"></asp:Label>
+                   <asp:DropDownList ID="ddlCPFouCNPJ" runat="server">
+                       <asp:ListItem>CPF</asp:ListItem>
+                       <asp:ListItem>CNPJ</asp:ListItem>
+                   </asp:DropDownList>
+                   <asp:TextBox ID="txtCPFouCNPJ" runat="server" CssClass="text" placeholder=""></asp:TextBox>
+               </div>
+
+               <div class="input-group w50">
+                   <asp:Label ID="lblSenha" runat="server" CssClass="label" Text="Digite sua senha"></asp:Label>
+                   <asp:TextBox ID="txtSenha" TextMode="Password" CssClass="text" runat="server" placeholder="Digite sua senha"></asp:TextBox>
+               </div>
+
+               <div class="input-group w50">
+                   <asp:Label ID="lblConfirma" runat="server" CssClass="label" Text="Confirmar senha"></asp:Label>
+                   <asp:TextBox ID="txtConfirma" TextMode="Password" CssClass="text" runat="server" placeholder="Confirme sua senha"></asp:TextBox>
+               </div>
+
+               <div class="input-group">
+                   <asp:Button ID="btnCadastro" cssclass="button" runat="server" Text="Cadastrar" />
+               </div>
+
+           </form>
+       </div>
+   </div>
 </body>
 </html>
 
--- prj_TCC/telas html/cadastro.aspx.designer.cs
+++ prj_TCC/telas html/cadastro.aspx.designer.cs
@@ -13,5 +13,122 @@ namespace prj_TCC.telas_html
 
     public partial class cadastro
     {
+
+        /// <summary>
+        /// form1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// lblNome control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblNome;
+
+        /// <summary>
+        /// txtNome control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNome;
+
+        /// <summary>
+        /// lblEmail control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblEmail;
+
+        /// <summary>
+        /// txtEmail control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEmail;
+
+        /// <summary>
+        /// lblCPFouCNPJ control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblCPFouCNPJ;
+
+        /// <summary>
+        /// ddlCPFouCNPJ control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlCPFouCNPJ;
+
+        /// <summary>
+        /// txtCPFouCNPJ control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCPFouCNPJ;
+
+        /// <summary>
+        /// lblSenha control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblSenha;
+
+        /// <summary>
+        /// txtSenha control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtSenha;
+
+        /// <summary>
+        /// lblConfirma control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblConfirma;
+
+        /// <summary>
+        /// txtConfirma control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtConfirma;
+
+        /// <summary>
+        /// btnCadastro control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnCadastro;
     }
 }
